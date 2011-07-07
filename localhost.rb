5a3369703badc87d3dc3b997c371b9b46ffddf51
vaamyob <yob@xyooj.com>
2011-07-06 21:42:19 -0400

    disable logging of successful requests

--- localhost.rb
+++ localhost.rb
@@ -1,11 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 configure do
-	set :bind, '127.0.0.1'
-	set :port, '80'
-	set :session, false
-	set :logging, false
+  set :bind, '127.0.0.1'
+  set :port, '80'
 end
+disable :logging
 get'/*' do
-	""
+  ""
 end
